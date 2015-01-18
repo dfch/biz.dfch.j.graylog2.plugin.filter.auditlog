a6834d33a3946e07bfce2dbcdee7d964e8dba467
Ronald Rink <ronald.rink@d-fens.net>
2015-01-18 15:25:22 +0100

    2015-01-18; rrink; CHG: some more testing with message correlation

--- src/main/java/biz/dfch/j/graylog2/plugin/filter/dfchBizAuditLog.java
+++ src/main/java/biz/dfch/j/graylog2/plugin/filter/dfchBizAuditLog.java
@@ -1,7 +1,9 @@
 package biz.dfch.j.graylog2.plugin.filter;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -40,18 +42,88 @@ public class dfchBizAuditLog implements MessageFilter
     private ScriptEngine _scriptEngine;
     private ScriptContext _scriptContext;
     private File _file;
-
+    
     private static final Logger LOG = LoggerFactory.getLogger(dfchBizAuditLog.class);
+
+    private static final Object _lock = new Object();
+    private static Message _previousMessage;
+    private static List<Message> _messages = new ArrayList<Message>(1000);
 
     
     @Override
     public boolean filter(Message msg)
     {
-        System.out.printf("*** %s: '%s'\r\n", msg.getId(), msg.getMessage());
-        LOG.trace("*** %s: '%s'\r\n", msg.getId(), msg.getMessage());
+        //System.out.printf("*** %s: '%s'\r\n", msg.getId(), msg.getMessage());
+        //LOG.trace("*** %s: '%s'\r\n", msg.getId(), msg.getMessage());
         msg.addField("DF_PLUGIN_NAME", DF_PLUGIN_NAME);
-        
-        
+
+        Thread t = Thread.currentThread();
+        //System.out.printf("*** _messages.size() %d ***\r\n", _messages.size());
+        System.out.printf("*** %s ***  %s @ %x: '%s'\r\n", DF_PLUGIN_NAME, msg.getId(), t.getId(), msg.getMessage());
+        synchronized (_lock)
+        {
+            try
+            {
+                if(1000 <= _messages.size())
+                {
+                    _messages.remove(0);
+                }
+                _messages.add(msg);
+                // get avg of all messages
+                double cpuAverage = 0;
+                for(Message entry : _messages)
+                {
+                    Number cpu = (Number) entry.getField("cpu");
+                    if(null == cpu)
+                    {
+                        continue;
+                    }
+                    cpuAverage += cpu.doubleValue();
+                }
+                cpuAverage /= _messages.size();
+                System.out.printf("cpuAverage over the last %d messages: %f\r\n", _messages.size(), cpuAverage);
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+
+//        if(null != _previousMessage)
+//        {
+//            System.out.printf("*** DF_PLUGIN_NAME *** PREVIOUS *** %s @ %x: '%s'\r\n", _previousMessage.getId(), t.getId(), _previousMessage.getMessage());
+//            try
+//            {
+//                Map<String, Object> fields = _previousMessage.getFields();
+//                for(Map.Entry<String, Object> entry : fields.entrySet())
+//                {
+//                    System.out.printf("Key '%s', Value '%s'\r\n", entry.getKey(), entry.getValue().toString());
+//                }
+//            }
+//            catch(Exception ex)
+//            {
+//                ex.printStackTrace();
+//            }
+//        }
+//        System.out.printf("*** DF_PLUGIN_NAME *** CURRENT  *** %s @ %x: '%s'\r\n", msg.getId(), t.getId(), msg.getMessage());
+//        try
+//        {
+//            Map<String, Object> fields = msg.getFields();
+//            //_previousMessage = new Message(fields);
+//            synchronized (_lock)
+//            {
+//                _previousMessage = msg;
+//            }
+//            for(Map.Entry<String, Object> entry : fields.entrySet())
+//            {
+//                System.out.printf("Key '%s', Value '%s'\r\n", entry.getKey(), entry.getValue().toString());
+//            }
+//        }
+//        catch(Exception ex)
+//        {
+//            ex.printStackTrace();
+//        }
+
         // TODO 
         // add your code to forward messages to where it is needed
         
